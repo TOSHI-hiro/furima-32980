30a4d9fee9259684f629971d78a0ae4d0d9d07db
TOSHI-hiro <inchs528@gmail.com>
2021-01-17 19:42:47 +0900

    新規機能/ログイン/ログアウトの実装
    各ファイル毎に編集しました。
    すみません。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def index
-  end
 
-  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:first_name ,:last_name,:first_name_kana,:last_name_kana,:date_of_birth])
+  end 
 end
