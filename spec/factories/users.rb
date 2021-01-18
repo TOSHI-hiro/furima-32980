c630befedd46fcb58441567c6e77b4a7dbed5bae
TOSHI-hiro <inchs528@gmail.com>
2021-01-18 22:10:34 +0900

    factory_botの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,8 @@ FactoryBot.define do
    factory :user do 
     name {"aaaaaa"}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password {"hoge12"}
+    password_confirmation {"hoge12"}
     first_name {"あ"}
     last_name {"あ"}
     first_name_kana {"ア"}
