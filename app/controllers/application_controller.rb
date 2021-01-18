6d1743c3ec2c761e345b86655189d3c3de9aee6e
TOSHI-hiro <inchs528@gmail.com>
2021-01-18 19:26:43 +0900

    ログイン機能の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:first_name ,:last_name,:first_name_kana,:last_name_kana,:date_of_birth])
+
   end 
 end
