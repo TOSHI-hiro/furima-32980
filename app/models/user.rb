e9b7113db77151aff347ae6cd58165f64148b8bd
TOSHI-hiro <inchs528@gmail.com>
2021-01-18 20:13:18 +0900

    validationの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-  validates :name, presence: true, length: { maximum: 6 }
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
-  validates :date_of_birth, presence: true
-
+  with_options presence: true do
+    validates :name, length: { maximum: 6 }
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :date_of_birth
+  end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
