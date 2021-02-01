3c0b37bad4455d39dd2b665912545a002f5f6074
TOSHI-hiro <inchs528@gmail.com>
2021-02-01 20:31:57 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -59,6 +59,35 @@ RSpec.describe Item do
         expect(@item.errors.full_messages).to include("Deadline is not a number")
       end
 
+      it "category_idが1の場合は保存できないこと" do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it "status_idが1の場合は保存できないこと" do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+
+      it "shipping_idが１の場合は保存できないこと" do
+        @item.shipping_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping must be other than 1")
+
+      end
+      it "prefecture_idが１の場合は保存できないこと" do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+
+      it "deadline_idが１の場合は保存できないこと" do
+        @item.deadline_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Deadline must be other than 1")
+      end
+      
       it "priceがない場合は保存できないこと" do
         @item.price = ""
         @item.valid?
@@ -72,7 +101,7 @@ RSpec.describe Item do
       end
 
       it "priceが9,999,999以上の場合は保存できないこと" do
-        @item.price = "100000000"
+        @item.price = 100000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than 10000000")
       end
