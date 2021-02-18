b44c21cd9ce6f73700ef326e9ad62f17d97a3638
TOSHI-hiro <inchs528@gmail.com>
2021-02-18 11:18:04 +0900

    修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item do
-  factory :user do
+
     product_name           {Faker::Games::Zelda.item}
     subscription      {Faker::Games::Zelda.game}
     category_id     {2}
@@ -10,11 +10,10 @@ FactoryBot.define do
     deadline_id     {2}
     price           {Faker::Number.number(digits: 4)}
 
-
+    association :user
 
      after(:build) do |item|
        item.image.attach(io: File.open('public/images/comment.png'), filename: 'comment.png')
-     end
     end
   end
 end
